098abdad0bd0ea117ca4c3e8ad368f59a63b94b8
CloudyFog <razvanspataru150@gmail.com>
2025-12-14 18:57:52 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,8 +1,26 @@
 <?php
+$becuri = [
+    "dormitor" => false,
+    "baie"     => false, 
+    "bucatarie" => false,
+    "living"   => false
+];
+   
+header('Content-Type: application/json');
 if(isset($_GET['users'])){
-    echo "show all users"; 
+    echo "User aparut"; 
 }
-if(isset($_GET['id'])){
-    echo "show user by id"; 
+if(isset($_POST['dormitor'])){
+$becuri['dormitor'] = true;
 }
+if(isset($_POST['baie'])){
+$becuri['baie'] = true;
+}
+if(isset($_POST['bucatarie'])){
+$becuri['bucatarie'] = true;
+}
+if(isset($_POST['living'])){
+$becuri['living'] = true;
+}
+echo json_encode($becuri);
 ?>
