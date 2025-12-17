1a3987f390788f82ab8edc326078dd80e26468d2
CloudyFog <razvanspataru150@gmail.com>
2025-12-17 11:38:20 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -6,13 +6,13 @@ $Table = 'light_states';
 $Table_Log = 'light_log';
 $id_state = 1;
 
-if(isset($_POST['action'])) {
+if(isset($_POST['actiune'])) {
     
     $sql = "";
     $room_name_to_log = null; 
     $is_individual_toggle = false; 
 
-    switch ($_POST['action']) {
+    switch ($_POST['actiune']) {
         case 'Open':
             $sql = "UPDATE $Table SET bedroom = 1, bathroom = 1, kitchen = 1, living = 1 WHERE id = :id";
             break;
@@ -26,7 +26,7 @@ if(isset($_POST['action'])) {
         case 'bathroom':
         case 'kitchen':
         case 'living':
-            $room_name_to_log = $_POST['action'];
+            $room_name_to_log = $_POST['actiune'];
             $is_individual_toggle = true; 
             $sql = "UPDATE $Table SET {$room_name_to_log} = 1 - {$room_name_to_log} WHERE id = :id";
             break;
@@ -64,17 +64,17 @@ if(isset($_POST['action'])) {
     }
 }
 
-if (isset($_GET['statuses'])) {
-    $rooms = ['bedroom', 'bathroom', 'kitchen', 'living'];
-    $total_durations = [];
+if (isset($_GET['statusuri'])) {
+    $camere = ['bedroom', 'bathroom', 'kitchen', 'living'];
+    $durate_totale = [];
     $sql_select = "SELECT bedroom, bathroom, kitchen, living FROM $Table WHERE id = :id";
     try {
         $stmt_select = $pdo->prepare($sql_select);
         $stmt_select->bindParam(':id', $id_state, PDO::PARAM_INT);
         $stmt_select->execute();
-        $states_db = $stmt_select->fetch(PDO::FETCH_ASSOC);
-        $final_result = [];
-        foreach ($rooms as $room) {
+        $stari_db = $stmt_select->fetch(PDO::FETCH_ASSOC);
+        $rezultat_final = [];
+        foreach ($camere as $camera) {
             $sql_duration = "
                 SELECT 
                     SUM(TIMESTAMPDIFF(SECOND, l1.time, 
@@ -89,21 +89,21 @@ if (isset($_GET['statuses'])) {
             ";
             
             $stmt_duration = $pdo->prepare($sql_duration);
-            $stmt_duration->bindParam(':room', $room);
+            $stmt_duration->bindParam(':room', $camera);
             $stmt_duration->execute();
             $result = $stmt_duration->fetch(PDO::FETCH_ASSOC);
             $total_seconds = (int)$result['total_seconds_on'];
             $hours = floor($total_seconds / 3600);
             $minutes = floor(($total_seconds % 3600) / 60);
             $seconds = $total_seconds % 60;
-            $final_result[$room] = [
-                'status' => (bool)$states_db[$room],
+            $rezultat_final[$camera] = [
+                'status' => (bool)$stari_db[$camera],
                 'total_time_on' => sprintf("%02dh %02dm %02ds", $hours, $minutes, $seconds),
                 'total_seconds' => $total_seconds
             ];
         }
         
-        echo json_encode($final_result);
+        echo json_encode($rezultat_final);
 
     } catch (PDOException $e) {
     }
